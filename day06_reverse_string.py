64275fec2fba1b7664dacb387fee941c17fc2a81
Priya <iampriya20@gmail.com>
2025-06-29 10:09:03 -0500

    Day 6: Reverse a string using slicing

---
+++ day06_reverse_string.py
@@ -0,0 +1,10 @@
+# Day 6 - Reverse a String
+
+# Get input from the user
+text = input("Enter a word or sentence to reverse: ")
+
+# Reverse the string using slicing
+reversed_text = text[::-1]
+
+# Print the result
+print("Reversed string:", reversed_text)
